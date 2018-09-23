694bf372aedca5a4ab4352afe56f5bce050fdc59
Svyatoslav Lazarev <silazare.nsk@gmail.com>
2018-09-23 16:28:47 +0700

    Initial files addition

---
+++ README.md
@@ -0,0 +1,69 @@
+## Graph search algoritms script
+
+This script is prepared for undirected graph visiting round.
+Expected input is an undirected graph and node from which we should start to search.
+Some graph examples are attached.
+
+#### Usage description:
+```sh
+$ python3 graph.py <filename>.yml <start_node>
+```
+Where <filename>.yml - is YAML structured file format with graph description
+<start_node> - is node name from which we should start to search
+
+#### Functions description:
+`graph_parse(gfile)` - read and convert graph from input YAML file to dictionary
+
+**gfile** - input file in YAML format with dict file structure, where nodes are Keys and all edges from the node are list Items.
+Each node must have all its edges defined.
+Output returns dictionary with nodes and edges.
+
+`dfs(graph, start)` - depth-first search algorithm for graph.
+
+**graph** - input graph in dict format with nodes and edges.
+**start** - start node from which search should be started.
+Output returns visited nodes list, order is according DFS algorithm.
+
+`bfs(graph, start)` - breadth-first search algorithm for graph.
+
+**graph** - input graph in dict format with nodes and edges.
+**start** - start node from which search should be started.
+Output returns visited nodes list, order is according BFS algorithm.
+
+
+Example structure:
+
+Graph>
+ ```        
+         A
+       /   \
+      B--D--C
+       \ | /
+         E
+```
+Input file structure>
+```
+A:
+- B
+- C
+
+B:
+- A
+- D
+- E
+
+C:
+- A
+- D
+- E
+
+D:
+- B
+- C
+- E
+
+E:
+- B
+- C
+- D
+```
---
+++ graph.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+import sys
+import yaml
+from sys import argv
+from pprint import pprint
+
+def graph_parse(gfile):
+    '''read graph from yaml file'''
+    with open(gfile) as f:
+            graph_dict = yaml.load(f)
+    return graph_dict
+
+def dfs(graph, start):
+    '''depth-first search algorithm'''
+	visited = []
+    select = [start]
+    while select:
+        try:
+            node = select.pop(0)
+            if node not in visited:
+				#print('visited node -', node)
+                visited = visited+[node]
+                select = graph[node]+select
+        except KeyError:
+            print('Wrong graph, please check all node edges!')
+            sys.exit()
+	#print('====')
+    return visited
+
+def bfs(graph, start):
+  '''breadth-first search algorithm'''
+  visited = []
+  select = [start]
+  while select:
+      try:
+          node = select.pop(0)
+          if not node in visited:
+			  #print('visited node -', node)
+              visited = visited+[node]
+              select = select+graph[node]
+      except KeyError:
+            print('Wrong graph, please check all node edges!')
+            sys.exit()
+  #print('====')
+  return visited
+
+
+#Input parameters section
+input_file = ''.join(argv[1:2])
+start_node = ''.join(argv[2:3])
+
+if not input_file or not start_node:
+    print('Missing input parameters!')
+    print('Usage should be: python graph.py <filename>.yaml <start_node>')
+    sys.exit()
+    
+if __name__ == '__main__':
+    print('Converted input graph: ')
+    pprint(graph_parse(input_file))
+    print('Visited nodes in depth --', dfs(graph_parse(input_file), start_node))
+    print('Visited nodes in breadth --', bfs(graph_parse(input_file), start_node))
